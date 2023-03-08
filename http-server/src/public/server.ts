5841acf6c6882790e9d8573a532b6dc969f03ec2
acmve <acmverden@hotmail.com>
2023-03-08 11:30:57 +0100

    http-server

---
+++ http-server/src/public/server.ts
@@ -0,0 +1,63 @@
+import * as dotenv from 'dotenv';
+
+import * as http from 'http';
+import fs from 'fs';
+import url from 'url'; //to pass params
+import logger from '../utility/logger'
+
+dotenv.config({path:'./config.env'});
+console.log(process.env.PORT);
+// console.log(process.env.HOSTNAME);
+
+
+
+// Part 1
+const data = fs.readFileSync(`${__dirname}\\data.json`, 'utf-8');
+
+const server = http.createServer((req, res) => {
+
+    const {query, pathname, path, href, search} = url.parse(req.url!, true);
+    // Set responds header
+    res.writeHead(200, {"Content-Type": "text/html"});
+    // const pathname = req.url;
+
+    console.log(pathname)
+    fs.readFile(`${__dirname}/public/homepage.html`,'utf-8', (err, data) => {
+        res.end(data);
+    });
+
+    if(pathname === '/') {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.readFile(`${__dirname}/public/homepage.html`,'utf-8', (err, data) => {
+            res.end(data);
+        });
+    }else if (pathname === "/about") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        logger.warn("WARNING ABOUT THE PAGE!")
+        res.end("<h1>About</h1>")
+
+
+    } else if(pathname === "/data") {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+        
+    } else if(pathname === "/date") {
+        console.log(query);
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(`Date: ${query.year}, ${query.month}, ${query.day}`);
+    } else {
+        res.writeHead(400, {"Content-type": "text/html"});
+        res.end(`Path ${pathname} does not exist.`)
+    }
+    // res.end("Hello World!");
+})
+
+server.listen({
+    host: process.env.HOSTNAME,
+    port: process.env.PORT,
+}, () =>  {
+    console.log(`Server is listening to http://localhost:${process.env.PORT}`)
+    logger.info(`Server is listening to http://localhost:${process.env.PORT}`)
+
+})
+
