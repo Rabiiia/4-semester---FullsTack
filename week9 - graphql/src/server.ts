53bf6fb355e42e7032fa3a916dd656301b0d0c3a
acmve <acmverden@hotmail.com>
2023-03-29 10:46:30 +0200

    forgot to push past weeks

---
+++ week9 - graphql/src/server.ts
@@ -0,0 +1,46 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import typeDefs from './schema';
+import books from './data'
+
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+    Mutation: {           //args
+      addBook: (_parent, {input}, _context) => {
+        const book = {
+          id: books.length+1,
+          title: input.title, //args.input.title
+          author: input.author
+        }
+        books.push(book);
+        return book;
+      },
+      deleteBook:(_parent, {id},_context) => {
+        const book = books.find(book => book.id === parseInt(id));
+        books.splice(books.indexOf(book), 1);
+        return book;
+      }
+    }
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
