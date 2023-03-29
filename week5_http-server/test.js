39331b183377ce097cb5bf308e053633e0d01b52
acmve <acmverden@hotmail.com>
2023-03-29 10:58:49 +0200

    without express

---
+++ week5_http-server/test.js
@@ -0,0 +1,9 @@
+// indbygget node
+console.log(__dirname); 
+console.log(__filename);
+
+// node variables
+console.log(module);
+console.log(arguments);
+
+// env variables. One place, reuse one variable
